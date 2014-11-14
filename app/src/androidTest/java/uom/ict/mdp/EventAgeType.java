33bd7df1e3197001feb262cf9dc91109421472ef
Miguel Muscat <miguelmuscat93@gmail.com>
2014-11-14 13:44:05 +0100

    Added friendly names for age type enums

--- app/src/androidTest/java/uom/ict/mdp/EventAgeType.java
+++ app/src/androidTest/java/uom/ict/mdp/EventAgeType.java
@@ -4,6 +4,17 @@ package uom.ict.mdp;
  * Event classification for people ages.
  */
 public enum EventAgeType {
-	ADULT,
-	CHILD;
+	GENERAL("General Audience"),
+	ADULT("Adults"),
+	CHILD("Children");
+
+	private String friendlyName;
+	private EventAgeType(String friendlyName) {
+		this.friendlyName = friendlyName;
+	}
+
+	@Override
+	public String toString() {
+		return this.friendlyName;
+	}
 }
