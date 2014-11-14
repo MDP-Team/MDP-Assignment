91f1a4243a97a4abb5b46dfdb47779dbf378d596
Miguel Muscat <miguelmuscat93@gmail.com>
2014-11-14 13:45:48 +0100

    Added Navigation Drawer

--- app/src/main/java/uom/ict/mdp/MainActivity.java
+++ app/src/main/java/uom/ict/mdp/MainActivity.java
@@ -2,18 +2,35 @@ package uom.ict.mdp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.ArrayAdapter;
 
 public class MainActivity extends Activity {
+
+	private String[] navDrawerEntries;
+	private DrawerLayout mDrawerLayout;
+	private ListView mDrawerList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
+		navDrawerEntries = getResources().getStringArray(R.array.nav_drawer_entries);
+		mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+		mDrawerList = (ListView) findViewById(R.id.left_drawer);
+
+		// Set the adapter for the list view
+		mDrawerList.setAdapter(new ArrayAdapter<String>(this,
+				android.R.layout.simple_list_item_1, navDrawerEntries));
+		// Set the list's click listener
+		mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+	}
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -33,4 +50,15 @@ public class MainActivity extends Activity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+	private class DrawerItemClickListener implements ListView.OnItemClickListener {
+		@Override
+		public void onItemClick(AdapterView parent, View view, int position, long id) {
+			selectItem(position);
+		}
+	}
+
+	/** Swaps fragments in the main content view */
+	private void selectItem(int position) {}
+
 }
