2c9d76c72c8497e09200cd3f953f916384820518
DodoSerebro <dodoserebro@gmail.com>
2014-12-15 02:21:40 +0100

    Added more layout designs, and fixed xml inconsistencies (changed it from the start). Still need to connect the intents and make the map work as I want it

---
+++ app/src/main/java/uom/ict/mdp/EventInfoAdapter.java
@@ -0,0 +1,61 @@
+package uom.ict.mdp;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.List;
+
+/**
+ * Created by DodoSerebro on 15/12/2014.
+ */
+
+/**
+ * Adapter Class to show the necessary information on screen.
+ */
+public class EventInfoAdapter extends ArrayAdapter<Events>{
+
+    public Context context;
+    public int resource;
+    public List<Events> objects;
+
+    public EventInfoAdapter(Context context, int resource, int textViewResourceId, List<Events> objects) {
+        super(context, resource, textViewResourceId, objects);
+
+        this.context = context;
+        this.resource = resource;
+        this.objects = objects;
+    }
+
+    public View getView (int position, View convertView, ViewGroup parent)
+    {
+        if (convertView == null)
+        {
+            LayoutInflater inflater = ((Activity) context).getLayoutInflater();
+            convertView = inflater.inflate(resource,parent,false);
+        }
+
+        TextView eventName = (TextView) convertView.findViewById(R.id.event_name_info);
+        eventName.setText("Event Name: " + objects.get(position).getName());
+
+        TextView streetName = (TextView) convertView.findViewById(R.id.street_name_info);
+        streetName.setText("Location:" + objects.get(position).getLocation());
+
+        TextView startTime = (TextView) convertView.findViewById(R.id.start_time_info);
+        startTime.setText("Start Time: " + objects.get(position).getTime());
+
+        TextView endTime = (TextView) convertView.findViewById(R.id.end_time_info);
+        endTime.setText("Start Time: " + objects.get(position).getTime());
+
+        TextView category = (TextView) convertView.findViewById(R.id.category_info);
+        category.setText("Category: " + objects.get(position).getAgeType());
+
+        return convertView;
+    }
+}
